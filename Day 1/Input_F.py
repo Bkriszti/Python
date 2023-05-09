883c261062232fc0f378003e2aaa438915ce0dbe
Bkriszti <118482105+Bkriszti@users.noreply.github.com>
2023-05-09 18:08:52 +0300

    Add files via upload

---
+++ Day 1/Input_F.py
@@ -0,0 +1,14 @@
+print("Day 1- Python Print Function \n" + "Angela")
+print("The function is declared like this: ")
+print("print ('what to print')")
+# name=input("What is your name?\n")
+# print(name)
+# print("Nice to meet you, ! " + input("What is your name?"))
+print(len(input("What is your name?")))
+a = input("a: ")
+b = input("b: ")
+aux = a
+a = b
+b = aux
+print("a: " + a)
+print("b: " + b)
---
+++ Day 1/main.py
@@ -0,0 +1,3 @@
+City=input("What is the name of the in which you grew up?\n")
+Pet=input("What is the name of your pet?\n")
+print("Your band name: "City+" "+Pet)
